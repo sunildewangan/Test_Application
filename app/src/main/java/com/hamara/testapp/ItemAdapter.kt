a2475e44923f05cfd751a74a3ca469e209ebf467
TavagaSunil <sunildewangan93@gmail.com>
2023-05-10 13:18:27 +0530

    add all file

---
+++ app/src/main/java/com/hamara/testapp/ItemAdapter.kt
@@ -0,0 +1,38 @@
+package com.hamara.testapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class ItemAdapter(var list:ArrayList<Int>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+
+    class ItemViewHolder(ItemView: View): RecyclerView.ViewHolder(ItemView) {
+
+        val image = ItemView.findViewById<ImageView>(R.id.image)
+        val textItem = ItemView.findViewById<TextView>(R.id.count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        Glide.with(holder.image).load(R.drawable.ic_baseline_sports_baseball_24).into(holder.image)
+        holder.textItem.text = list[position].toString()
+    }
+
+    override fun getItemCount(): Int  = list.size
+
+    fun updateItem(item:ArrayList<Int>){
+        this.list = item
+        notifyDataSetChanged()
+    }
+}
+
